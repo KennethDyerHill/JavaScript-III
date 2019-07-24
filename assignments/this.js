38479e4b66c8266f6254c17d53da9cfee16de679
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2019-07-24 18:09:51 -0400

    Finished this.js excercise 1 and 2

--- assignments/this.js
+++ assignments/this.js
@@ -1,20 +1,31 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding
+* 2. Implicit Binding
+* 3. New Binding
+* 4. Explicit Binding
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
+function animalNoise(noise) {
+   console.log(this);
+   return noise;
+}
+animalNoise('Moo!');
 // code example for Window Binding
 
 // Principle 2
-
+const snakes = {
+    food: 'Mice',
+    sound: function(snakeSound) {
+        console.log('My pet loves to eat ${food} and she loves to say ${this.snakeSound}');
+        console.log(this);
+    }
+};
+snakes.sound('Hiss!')
 // code example for Implicit Binding
 
 // Principle 3
